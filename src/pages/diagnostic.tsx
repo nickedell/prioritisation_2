0ac6aaafe49b193334dfb93cf07be30bef9a1417
Nick Edell <various@humble2.com>
2025-06-14 17:12:51 +0100

    mat ass setup 1

---
+++ src/pages/diagnostic.tsx
@@ -0,0 +1,15 @@
+// src/pages/DiagnosticPage.tsx
+
+import React from 'react';
+
+const DiagnosticPage = () => {
+    return (
+        <div className="text-white p-8">
+            <h1 className="text-3xl font-bold mb-4">Maturity Diagnostic</h1>
+            <p>This is the new page where the diagnostic tool and spider chart will be built.</p>
+            {/* We will add a link to the next page here soon */}
+        </div>
+    );
+};
+
+export default DiagnosticPage;
