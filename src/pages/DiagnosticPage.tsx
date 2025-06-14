7c26017de6f5090890a8b9dd5916102884ac66b0
Nick Edell <various@humble2.com>
2025-06-14 17:20:59 +0100

    Create DiagnosticPage.tsx

---
+++ src/pages/DiagnosticPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DiagnosticPage = () => {
+    return (
+        <div className="text-white p-8">
+            <h1 className="text-3xl font-bold mb-4">Maturity Diagnostic</h1>
+            <p>This is the new page where the diagnostic tool and spider chart will be built.</p>
+        </div>
+    );
+};
+
+export default DiagnosticPage;
