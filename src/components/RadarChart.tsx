b1b23665fa19a8968f1c9b15d2a342103f55bc28
Nick Edell <various@humble2.com>
2025-06-16 00:37:22 +0100

    x

--- src/components/RadarChart.tsx
+++ src/components/RadarChart.tsx
@@ -13,11 +13,10 @@ interface RadarChartComponentProps {
 
 const RadarChartComponent: React.FC<RadarChartComponentProps> = ({ data }) => {
     return (
-        // UPDATE: Changed height from 400 to 300
         <ResponsiveContainer width="100%" height={300}>
             <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
                 <PolarGrid stroke="#555" />
-                <PolarAngleAxis dataKey="subject" stroke="#fff" />
+                <PolarAngleAxis dataKey="subject" stroke="#fff" tickLine={false} />
                 <PolarRadiusAxis angle={30} domain={[0, 5]} tickCount={6} stroke="#555" />
                 <Radar name="Maturity Score" dataKey="score" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
             </RadarChart>
