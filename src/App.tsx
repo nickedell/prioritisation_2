61273f3d9237343673c06a936562230e452f571c
Nick Edell <various@humble2.com>
2025-06-20 18:01:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MaturityProvider } from './context/MaturityContext';
 import DiagnosticPage6 from './pages/DiagnosticPage6';
-import PrioritisationResults from './components/PrioritisationResults'; // Assuming this is the correct path
 
 function App() {
   return (
-    // The MaturityProvider wraps your entire application
+    // The MaturityProvider wraps your pages, making the context available
     <MaturityProvider>
       <Router>
         <Routes>
           <Route path="/" element={<DiagnosticPage6 />} />
-          {/* We will fix the Prioritisation page in a later step */}
-          <Route path="/prioritisation" element={<div>Prioritisation Page Placeholder</div>} />
+          {/* Add other routes here later as needed */}
         </Routes>
       </Router>
     </MaturityProvider>
